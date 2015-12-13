3abdd4c35f28200af47e775bcb9c15b6dc8f3e0f
Arduino Enigma <arduinoenigma@gmail.com>
2015-12-13 09:12:41 -0500

    Create TouchScreenDebouncingDemo.ino

---
+++ TouchScreenDebouncingDemo.ino
@@ -0,0 +1,90 @@
+#include <TFTv2.h>
+#include <SPI.h>
+#include <SeeedTouchScreen.h>
+
+byte releasecount = 0;
+byte state = 0;
+bool pressed = false;
+
+//init TouchScreen port pins
+TouchScreen ts = TouchScreen(XP, YP, XM, YM);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Tft.TFTinit();  //init TFT library
+}
+
+void loop() {
+
+  Point p = ts.getPoint();
+  p.x = map(p.x, TS_MINX, TS_MAXX, 0, 240);
+  p.y = map(p.y, TS_MINY, TS_MAXY, 0, 320);
+
+  if (p.z > 1000)
+  {
+    Serial.print("Phantom touch at ");
+    Serial.print(p.x);
+    Serial.print(" ");
+    Serial.println(p.y);
+  }
+
+  // if no max detection phantom touches are detected
+  if ((p.z > __PRESURE) && (p.z < 1000))
+  {
+    pressed = true;
+  }
+  else
+  {
+    pressed = false;
+  }
+
+  switch (state)
+  {
+    case 0:
+      {
+        if (pressed)
+        {
+          // do entry tasks here
+
+          Serial.print("initial screen touch at ");
+          Serial.print(p.x);
+          Serial.print(" ");
+          Serial.println(p.y);
+
+          releasecount = 0;
+          state = 1;
+        }
+        break;
+      }
+
+    case 1:
+      {
+        if (!pressed)
+        {
+          Serial.println("no touch detected");
+          releasecount++;
+        }
+        else
+        {
+          // send x and y to task needing press and hold
+          Serial.print("screen touch at ");
+          Serial.print(p.x);
+          Serial.print(" ");
+          Serial.println(p.y);
+
+          releasecount = 0;
+        }
+
+        // adjust this threshold, or use timer
+        if (releasecount > 10)
+        {
+          // cleanup anything drawn
+          Serial.println("release registered");
+          state = 0;
+        }
+        break;
+      }
+  }
+}
